9fa5b00a098ba1b43ef6c3cfb8cffb587a917b06
dicki ferialdi hutasoit <dikiferialdihutasoit@gmail.com>
2025-06-14 01:44:21 +0700

    edit web.php

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\SurveyController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -12,7 +12,7 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
-
+Route::post('/survey/store', [SurveyController::class, 'store'])->name('survey.store');
 Route::get('/', function () {
     return view('welcome');
 });
